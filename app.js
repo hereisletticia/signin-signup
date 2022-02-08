a595037dcbf863315fe01dea888a7ee1ccb907ac
hereisletticia <hereisletticia@outlook.com>
2022-02-08 13:10:49 -0300

    6th commit: app.js DONE

---
+++ app.js
@@ -0,0 +1,11 @@
+const sing_in_btn = document.querySelector('#sign-in-button');
+const sign_up_btn = document.querySelector('#sign-up-button');
+const container = document.querySelector('.container');
+
+sign_up_btn.addEventListener('click', ()=>{
+    container.classList.add('sign-up-mode');
+});
+
+sing_in_btn.addEventListener('click', ()=>{
+    container.classList.remove('sign-up-mode');
+});
